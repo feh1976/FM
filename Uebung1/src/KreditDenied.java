cfe05efda82854a4dac1305c7c561e011418086c
Philipp <dittmann@tzi.de>
2011-05-24 13:09:39 +0200

    added missing files

---
+++ Uebung1/src/KreditDenied.java
@@ -0,0 +1,6 @@
+/**
+ * Diese Fehlermeldung wird geworfen, wenn auf Grund der Bonität ein Kredit verwehrt wurde.
+ */
+public class KreditDenied extends Exception {
+  private static final long serialVersionUID = 2L;
+}
---
+++ Uebung1/src/LimitReached.java
@@ -0,0 +1,6 @@
+/**
+ * Fehler, der geworfen wird, wenn versucht wird Geld abzuheben, aber das Limit erreicht wurde.
+ */
+public class LimitReached extends Exception {
+  private static final long serialVersionUID = 1L;
+}
